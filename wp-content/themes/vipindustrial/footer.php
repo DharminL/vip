30ada3841af15bd80ee35b5ea06e4a0c9dbb4501
Dhaval Dahiya <dhruvrajdahiya@gmail.com>
2017-09-06 17:03:44 +0530

    footer completed

--- wp-content/themes/vipindustrial/footer.php
+++ wp-content/themes/vipindustrial/footer.php
@@ -15,11 +15,42 @@
 ?>
 
 		</div><!-- #content -->
-
+			<footer class="site-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col col-lg-4 col-md-3 col-xs-6">
+                    <div class="widget about-widget">
+                        <?php             
+                            dynamic_sidebar( 'first-footer-widget-area' );   
+                        ?>
+                    </div>
+                    </div>
+                    <div class="col col-lg-2 col-md-3 col-xs-6">
+                        <div class="widget site-map-widget">
+                            <?php dynamic_sidebar( 'second-footer-widget-area' ); ?>
+                        </div>
+                    </div>
+                    <div class="col col-lg-3 col-md-3 col-xs-6">
+                    <div class="widget about-widget">
+                    <?php dynamic_sidebar( 'third-footer-widget-area' );?>
+                    </div>
+                    </div>
+                    <div class="col col-lg-3 col-md-3 col-xs-6">
+						
+                        <div class="widget social-media-widget">
+                         <?php dynamic_sidebar( 'fourth-footer-widget-area' );?>
+                           
+							<?php vipindustrial_show_social_icons(); ?>
+                           
+                        </div>
+                    </div>
+                </div> <!-- end row -->
+            </div> <!-- end container -->
+        </footer>
 		<footer id="colophon" class="site-footer" role="contentinfo">
 			<div class="wrap">
 				<?php
-				get_template_part( 'template-parts/footer/footer', 'widgets' );
+				
 
 				if ( has_nav_menu( 'social' ) ) : ?>
 					<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'vipindustrial' ); ?>">
@@ -35,7 +66,7 @@
 					</nav><!-- .social-navigation -->
 				<?php endif;
 
-				get_template_part( 'template-parts/footer/site', 'info' );
+				//get_template_part( 'template-parts/footer/site', 'info' );
 				?>
 			</div><!-- .wrap -->
 		</footer><!-- #colophon -->
